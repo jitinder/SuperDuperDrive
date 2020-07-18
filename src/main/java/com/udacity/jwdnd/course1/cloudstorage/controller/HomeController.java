5534bd4619f8a39494a9f738ca1f0f0ba7e4e811
Sidak <sidak.pasrichassp@gmail.com>
2020-07-18 20:32:10 +0530

    Implement Note Add/Update/Delete

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,19 +1,17 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.services.StorageService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,25 +20,34 @@ import java.util.List;
 public class HomeController {
 
     private StorageService storageService;
+    private NoteService noteService;
     private UserService userService;
 
-    public List<File> fileList;
+    private List<File> fileList;
+    private List<Note> noteList;
 
-    public HomeController(UserService userService, StorageService storageService){
+    public HomeController(UserService userService, StorageService storageService, NoteService noteService){
         this.userService = userService;
         this.storageService = storageService;
+        this.noteService = noteService;
     }
 
     @PostConstruct
     public void postConstruct(){
         fileList = new ArrayList<>();
+        noteList = new ArrayList<>();
     }
 
     @GetMapping
     public String getHome(Model model, Authentication authentication){
         int userId = userService.getUserId(authentication.getName());
+
         fileList = storageService.getFilesForUser(userId);
+        noteList = noteService.getNotes(userId);
+
         model.addAttribute("fileList", fileList);
+        model.addAttribute("noteList", noteList);
+
         return "home";
     }
 }
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NotesMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NotesMapper.java
@@ -1,8 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -10,5 +9,23 @@ import java.util.List;
 public interface NotesMapper {
 
     @Select("select * from NOTES where userid = #{userId}")
-    List<Note> getNotesByUser(int userId);
+    public List<Note> getNotesByUser(int userId);
+
+    @Select("select * from NOTES where userid = #{userId} and noteTitle = #{noteTitle}")
+    public Note getNote(String noteTitle, int userId);
+
+    @Select("select * from NOTES where noteid = #{noteId}")
+    public Note getNoteById(int noteId);
+
+    @Insert("insert into NOTES(notetitle, notedescription, userid) " +
+            "values(#{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    public int addNote(Note note);
+
+    @Update("update NOTES set notetitle=#{noteTitle}, noteDescription=#{noteDescription}" +
+            " where noteid=#{noteId}")
+    public int updateNote(Note note);
+
+    @Delete("delete from NOTES where noteid = #{noteId}")
+    public int deleteNoteById(int noteId);
 }
